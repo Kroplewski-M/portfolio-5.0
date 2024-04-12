5a9e6f7eebba8ecd245456dee8dcda23d8072bab
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-04-12 20:10:16 +0100

    added nav and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { NavBar } from "./components/NavBar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex">
+        <div className="">
+          <NavBar />
+        </div>
+        <div className="bg-[#333333] w-[100vw]"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+export const NavBar = () => {
+  return (
+    <nav className="w-[100px] h-[100vh] bg-[#1E1E1E] drop-shadow-xl">
+      <div className="hover:cursor-pointer">
+        <img src="./Logo.png" alt="logo" className="w-[100%]" />
+      </div>
+    </nav>
+  );
+};
