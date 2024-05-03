96d2fb79110d4c771484cd4aa391d260076f0eb7
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-05-03 19:59:43 +0100

    changing icon colors on hover

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -34,6 +34,18 @@ export const NavBar = () => {
     }
   }, [windowSize]);
 
+  //Icon states
+  const [homeHover, setHomeHover] = useState(false);
+  const [aboutHover, setAboutHover] = useState(false);
+  const [skillsHover, setSkillsHover] = useState(false);
+  const [projectsHover, setProjectsHover] = useState(false);
+  const [contactHover, setContactHover] = useState(false);
+
+  const handleHomeHover = () => setHomeHover(!homeHover);
+  const handleAboutHover = () => setAboutHover(!aboutHover);
+  const handleSkillsHover = () => setSkillsHover(!skillsHover);
+  const handleProjectHover = () => setProjectsHover(!projectsHover);
+  const handleContactHover = () => setContactHover(!contactHover);
   return (
     <div className="z-50 relative sticky top-0 left-0">
       {openNav ? (
@@ -51,20 +63,20 @@ export const NavBar = () => {
             <img src="./Logo.png" alt="logo" className="w-[100%]" />
           </div>
           <div className="flex flex-col w-[33px] mx-auto space-y-7 mt-16">
-            <div className="hover:cursor-pointer">
-              <HomeSVG width={iconSize} height={iconSize} color="#fff" />
+            <div className="hover:cursor-pointer" onMouseEnter={handleHomeHover} onMouseLeave={handleHomeHover}>
+              <HomeSVG width={iconSize} height={iconSize} color={homeHover ? "#A020F0" : "#fff"} />
             </div>
-            <div className="hover:cursor-pointer">
-              <AboutSVG width={iconSize} height={iconSize} color="#fff" />
+            <div className="hover:cursor-pointer" onMouseEnter={handleAboutHover} onMouseLeave={handleAboutHover}>
+              <AboutSVG width={iconSize} height={iconSize} color={aboutHover ? "#A020F0" : "#fff"} />
             </div>
-            <div className="hover:cursor-pointer">
-              <SkillsSVG width={iconSize} height={iconSize} color="#fff" />
+            <div className="hover:cursor-pointer" onMouseEnter={handleSkillsHover} onMouseLeave={handleSkillsHover}>
+              <SkillsSVG width={iconSize} height={iconSize} color={skillsHover ? "#A020F0" : "#fff"} />
             </div>
-            <div className="hover:cursor-pointer">
-              <ProjectsSVG width={iconSize} height={iconSize} color="#fff" />
+            <div className="hover:cursor-pointer" onMouseEnter={handleProjectHover} onMouseLeave={handleProjectHover}>
+              <ProjectsSVG width={iconSize} height={iconSize} color={projectsHover ? "#A020F0" : "#fff"} />
             </div>
-            <div className="hover:cursor-pointer">
-              <ContactSVG width={iconSize} height={iconSize} color="#fff" />
+            <div className="hover:cursor-pointer" onMouseEnter={handleContactHover} onMouseLeave={handleContactHover}>
+              <ContactSVG width={iconSize} height={iconSize} color={contactHover ? "#A020F0" : "#fff"} />
             </div>
           </div>
         </nav>
