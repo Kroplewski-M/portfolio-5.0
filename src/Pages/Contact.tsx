1409abdf8b59f5cf832d3d116a92e8966dbd4e41
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-05-13 18:39:32 +0100

    minor stying changes

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,9 +1,11 @@
 export const Contact = () => {
     return (
-        <section className="w-[100%] min-h-[100vh] z-[20] bg-[#333333] relative" id="contact">
-            <h1 className="text-[50px] font-bold text-gray-200">
-                <span className="bg-purple-600 p-[7px] rounded-md">Contact</span>
-            </h1>
+        <section className="w-[100%] min-h-[100vh] z-[20] bg-[#222222] relative pt-5" id="contact">
+            <div className="w-[100%] z-50 relative pt-5 md:pl-16 pl-6">
+                <h1 className="text-[50px] font-bold text-gray-200">
+                    <span className="bg-purple-600 p-[7px] rounded-md">Contact</span>
+                </h1>
+            </div>
         </section>
     );
 };
